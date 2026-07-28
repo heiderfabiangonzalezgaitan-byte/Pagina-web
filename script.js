f6b268c3066159a7f17e5c1e879045344debf9f5
Fabian Gonzalez <heiderfabiangonzalezgaitan@gmail.com>
2026-07-27 21:41:51 -0500

    Update script.js

--- script.js
+++ script.js
@@ -32,7 +32,7 @@
         // "https://tu-nueva-foto-aqui.jpg",
       ],
       fotografia: [
-        "https://images.unsplash.com/photo-1526170375885-4d8ecf77b99f?auto=format&fit=crop&w=900&q=80",
+        "https://raw.githubusercontent.com/heiderfabiangonzalezgaitan-byte/Pagina-web/refs/heads/main/IMG/1.jpeg",
         "https://images.unsplash.com/photo-1526170375885-4d8ecf77b99f?auto=format&fit=crop&w=900&q=80",
         "https://images.unsplash.com/photo-1560472354-b33ff0c44a43?auto=format&fit=crop&w=900&q=80",
         "https://images.unsplash.com/photo-1516035069371-29a1b244cc32?auto=format&fit=crop&w=900&q=80",
