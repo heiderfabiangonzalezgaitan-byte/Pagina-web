b4982ab56e939fb4880c81cc9e78ae385fa8bfad
Fabian Gonzalez <heiderfabiangonzalezgaitan@gmail.com>
2026-07-28 20:54:10 -0500

    Update script.js

--- script.js
+++ script.js
@@ -40,6 +40,8 @@
         "https://raw.githubusercontent.com/heiderfabiangonzalezgaitan-byte/Pagina-web/refs/heads/main/IMG/12.jpeg",
         "https://raw.githubusercontent.com/heiderfabiangonzalezgaitan-byte/Pagina-web/refs/heads/main/IMG/15.jpeg",
         "https://raw.githubusercontent.com/heiderfabiangonzalezgaitan-byte/Pagina-web/refs/heads/main/IMG/20.jpeg",
+        "https://raw.githubusercontent.com/heiderfabiangonzalezgaitan-byte/Pagina-web/refs/heads/main/IMG/23.jpeg"
+        "
       
       ],
       paquete: [
