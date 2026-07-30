7be2f378e0b6e663c71dda56bd1e9d198c167c14
Fabian Gonzalez <heiderfabiangonzalezgaitan@gmail.com>
2026-07-29 22:10:35 -0500

    Update script.js

--- script.js
+++ script.js
@@ -15,7 +15,7 @@
       ],
       flyers: [
         "https://raw.githubusercontent.com/heiderfabiangonzalezgaitan-byte/Pagina-web/refs/heads/main/IMG/Flyers%201.png",
-        "https://images.unsplash.com/photo-1561070791-2526d30994b5?auto=format&fit=crop&w=900&q=80",
+        "https://raw.githubusercontent.com/heiderfabiangonzalezgaitan-byte/Pagina-web/refs/heads/main/IMG/Flyers%202.png",
         "https://images.unsplash.com/photo-1531403009284-440f080d1e12?auto=format&fit=crop&w=900&q=80"
         // "https://tu-nueva-foto-aqui.jpg",
       ],
